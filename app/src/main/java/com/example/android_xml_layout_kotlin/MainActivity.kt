58c325369657af72dd7f877160e36c0797dbcf0f
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-03-18 04:26:00 -0700

    add two number [Done]

--- app/src/main/java/com/example/android_xml_layout_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/android_xml_layout_kotlin/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.android_xml_layout_kotlin
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,11 +13,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(R.layout.add_two_num)
+        val num1 = findViewById<EditText>(R.id.num1)
+        val num2 = findViewById<EditText>(R.id.num2)
+        val add = findViewById<Button>(R.id.button)
+        val result = findViewById<TextView>(R.id.result)
+
+        add.setOnClickListener{
+            val a = num1.text.toString().toIntOrNull();
+            val b = num2.text.toString().toIntOrNull();
+            if(a != null && b != null){
+                val c= a + b
+                result.text = c.toString()
+            }else{
+                result.text = "Enter two number"
+            }
+
         }
+
     }
 }
