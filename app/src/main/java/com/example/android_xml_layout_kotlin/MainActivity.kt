2f6ffdca3fe75935051ee194fb8bea7d5c71ac7a
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-04-03 09:19:52 -0700

    Task Three UI [Done]

--- app/src/main/java/com/example/android_xml_layout_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/android_xml_layout_kotlin/MainActivity.kt
@@ -13,9 +13,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-
-
+        setContentView(R.layout.task_three)
 
     }
 }
